a9b8d4536cce295057ae2626401d847138a5c411
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-25 05:11:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   counter = 0
   while counter < array.length do
     squared = array[counter] ** 2
-    squared_array << square
+    squared_array << squared
     counter += 1
   end
   squared_array
